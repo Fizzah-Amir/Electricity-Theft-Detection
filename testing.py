2debf2fee5b8bbdcf3059597c9636222cb6eef1d
Fizzah-Amir <bscs24031@itu.edu.pk>
2026-05-16 01:56:50 -0700

    evaluation import+output dir made

--- testing.py
+++ testing.py
@@ -17,3 +17,14 @@ print(f"Test samples    : {len(X_test)}")
 print(f"Actual theft    : {y_test.sum()} cases")
 print(f"Actual normal   : {(y_test==0).sum()} cases")
 print("Load successfull!")
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score,
+    f1_score, roc_auc_score, confusion_matrix,
+    classification_report
+)
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+os.makedirs("gb_scratch_testing", exist_ok=True)
